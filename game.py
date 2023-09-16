8241370ecc4394f1acc2652bbb62250cfa7c803e
Chefox <Chefoxee@yandex.ru>
2023-09-16 19:13:54 +0300

    First commit

---
+++ game.py
@@ -0,0 +1,25 @@
+from map import Map
+from player import Player
+
+
+class Game:
+    def __init__(self):
+        self.map = Map()
+        self.player = Player()
+        # Записываем текущее состояние игры
+        pass
+
+    def run(self):
+        self.step()
+
+    def step(self):
+        self.display_actual_info()
+        # Запрашиваем у игрока его решение
+        # Применяем его решение
+        pass
+
+    def display_actual_info(self):
+        print(f"Вы находиесь в локации: {self.map.get_current_environment()}")
+        passages = self.map.get_passages()
+        for direction, environment in passages.items():
+            print(f"На {direction} находится {environment}")
---
+++ map.py
@@ -0,0 +1,82 @@
+from enum import Enum, auto
+from typing import List
+
+
+class Environment(Enum):
+    FOREST = auto()
+    DESERT = auto()
+
+
+class Direction(Enum):
+    NORTH = auto()
+    SOUTH = auto()
+    EAST = auto()
+    WEST = auto()
+
+
+ENV_TO_RUS = {
+    Environment.FOREST: "Лес",
+    Environment.DESERT: "Пустыня",
+}
+
+
+class Location:
+
+    def __init__(self, environment: Environment):
+        self.environment = environment
+
+
+class Point:
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Map:
+
+    def __init__(self):
+        self.map_size = 2
+        self.zones: List[List[Location]] = [
+            [Location(Environment.FOREST),
+             Location(Environment.DESERT),
+             ],
+            [Location(Environment.FOREST),
+             Location(Environment.DESERT),
+             ]
+        ]
+        self.position = Point(0, 0)
+
+    def get_current_environment(self) -> str:
+        x = self.position.x
+        y = self.position.y
+        env = self.zones[y][x].environment
+        return ENV_TO_RUS[env]
+
+    def get_passages(self) -> dict[Direction, Environment]:
+        passages: dict[Direction, Environment] = {}
+
+        x = self.position.x
+        y = self.position.y
+        north_y = y - 1
+        south_y = y + 1
+        west_x = x - 1
+        east_x = x + 1
+
+        if west_x >= 0:
+            west_env = self.zones[y][west_x].environment
+            passages[Direction.WEST] = west_env
+
+        if east_x < self.map_size:
+            east_env = self.zones[y][east_x].environment
+            passages[Direction.EAST] = east_env
+
+        if north_y >= 0:
+            north_env = self.zones[north_y][x].environment
+            passages[Direction.NORTH] = north_env
+
+        if south_y < self.map_size:
+            south_env = self.zones[south_y][x].environment
+            passages[Direction.SOUTH] = south_env
+
+        return passages
